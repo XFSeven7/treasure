7ec13f2d25e0ff4fa2fae210087ecb2b0a3a44a7
seven <377289596@qq.com>
2016-07-14 10:51:55 +0800

    改为handler实现注册

--- app/src/main/java/com/archer/truesure/user/register/RegisterPresenter.java
+++ app/src/main/java/com/archer/truesure/user/register/RegisterPresenter.java
@@ -1,6 +1,7 @@
 package com.archer.truesure.user.register;
 
-import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.archer.truesure.net.NetOkHttpClient;
 import com.archer.truesure.user.entity.RegisterInfo;
@@ -11,7 +12,7 @@ import com.hannesdorfmann.mosby.mvp.MvpNullObjectBasePresenter;
 import java.io.IOException;
 
 import okhttp3.Call;
-import okhttp3.MediaType;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -23,99 +24,179 @@ import okhttp3.Response;
  */
 public class RegisterPresenter extends MvpNullObjectBasePresenter<RegisterView> {
 
-    private NetOkHttpClient netOkHttpClient;
+
+//    private NetOkHttpClient netOkHttpClient;
+//
+//    private Gson gson;
+//
+//    private RegisterInfo registerInfo;
+
+//    /**
+//     * 模拟注册
+//     */
+
+//    public void register(RegisterInfo registerInfo) {
+//        this.registerInfo = registerInfo;
+//        netOkHttpClient = NetOkHttpClient.getInstance();
+//        gson = new Gson();
+//        new MyAsyncTask().execute();
+//    }
+//
+//    private final class MyAsyncTask extends AsyncTask<Void, Void, RegisterResultInfo> {
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//        }
+//
+//        @Override
+//        protected RegisterResultInfo doInBackground(Void... params) {
+//
+//            OkHttpClient okHttpClient = netOkHttpClient.getOkHttpClient();
+//
+//            String content = gson.toJson(registerInfo);
+//
+//            MediaType type = MediaType.parse("truesure");
+//            RequestBody body = RequestBody.create(type, content);
+//
+//            Request request = new Request.Builder()
+//                    .post(body)
+//                    .url(NetOkHttpClient.APP_URL + "/Handler/UserHandler.ashx?action=register")
+//                    .build();
+//
+//            Call call = okHttpClient.newCall(request);
+//
+//            Response execute;
+//            String json;
+//            try {
+//                execute = call.execute();
+//                json = execute.body().string();
+//            } catch (IOException e) {
+//                return null;
+//            }
+//
+//            RegisterResultInfo registerResultInfo = gson.fromJson(json, RegisterResultInfo.class);
+//
+//            if (registerResultInfo != null) {
+//                return registerResultInfo;
+//            }
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(RegisterResultInfo registerResultInfo) {
+//            super.onPostExecute(registerResultInfo);
+//
+//            getView().hideProgress();
+//
+//            if (registerResultInfo == null) {
+//                getView().showMessage("未知错误");
+//                return;
+//            }
+//
+//            /*
+//
+//                "errcode": 1,                  //状态值
+//                "errmsg": "登录成功！",        //返回信息
+//                "tokenid": 171                 //用户令牌
+//
+//                "errcode":2,
+//                "errmsg":"注册此用户名已存在！"
+//
+//            */
+//
+//            switch (registerResultInfo.getCode()) {
+//
+//                case 1:
+//                    getView().showMessage(registerResultInfo.getMsg());
+//                    getView().NavigationToHome();
+//                    break;
+//                case 2:
+//                    getView().showMessage(registerResultInfo.getMsg());
+//                    break;
+//
+//            }
+//
+//        }
+//
+//    }
 
     private Gson gson;
 
-    private RegisterInfo registerInfo;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
 
-    /**
-     * 模拟注册
-     */
-    public void register(RegisterInfo registerInfo) {
-        this.registerInfo = registerInfo;
-        netOkHttpClient = NetOkHttpClient.getInstance();
+    private NetOkHttpClient netOkHttpClient;
+
+    public void register(RegisterInfo info) {
+
         gson = new Gson();
-        new MyAsyncTask().execute();
+
+        netOkHttpClient = NetOkHttpClient.getInstance();
+
+        OkHttpClient okHttpClient = netOkHttpClient.getOkHttpClient();
+
+        String s = gson.toJson(info);
+
+        RequestBody body = RequestBody.create(null, s);
+
+        final Request request = new Request.Builder()
+                .url(NetOkHttpClient.APP_URL + "/Handler/UserHandler.ashx?action=register")
+                .post(body)
+                .build();
+
+
+        Call call = okHttpClient.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                failure(e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                RegisterResultInfo registerResultInfo;
+                if (response.isSuccessful()) {
+                    String string = response.body().string();
+                    registerResultInfo = gson.fromJson(string, RegisterResultInfo.class);
+                    if (registerResultInfo.getCode() == 1) {
+                        success(registerResultInfo.getMsg());
+                        return;
+                    }
+                    failure(registerResultInfo.getMsg());
+                    return;
+                }
+                failure("未知错误");
+
+            }
+        });
+
     }
 
-    private final class MyAsyncTask extends AsyncTask<Void, Void, RegisterResultInfo> {
+    private void success(final String msg) {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected RegisterResultInfo doInBackground(Void... params) {
-
-            OkHttpClient okHttpClient = netOkHttpClient.getOkHttpClient();
-
-            String content = gson.toJson(registerInfo);
-
-            MediaType type = MediaType.parse("truesure");
-            RequestBody body = RequestBody.create(type, content);
-
-            Request request = new Request.Builder()
-                    .post(body)
-                    .url(NetOkHttpClient.APP_URL + "/Handler/UserHandler.ashx?action=register")
-                    .build();
-
-            Call call = okHttpClient.newCall(request);
-
-            Response execute;
-            String json;
-            try {
-                execute = call.execute();
-                json = execute.body().string();
-            } catch (IOException e) {
-                return null;
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                getView().hideProgress();
+                getView().showMessage(msg);
+                getView().NavigationToHome();
             }
+        });
 
-            RegisterResultInfo registerResultInfo = gson.fromJson(json, RegisterResultInfo.class);
+    }
 
-            if (registerResultInfo != null) {
-                return registerResultInfo;
+    private void failure(final String msg) {
+
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                getView().hideProgress();
+                getView().showMessage(msg);
             }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(RegisterResultInfo registerResultInfo) {
-            super.onPostExecute(registerResultInfo);
-
-            getView().hideProgress();
-
-            if (registerResultInfo == null) {
-                getView().showMessage("未知错误");
-                return;
-            }
-
-            /*
-
-                "errcode": 1,                  //状态值
-                "errmsg": "登录成功！",        //返回信息
-                "tokenid": 171                 //用户令牌
-
-                "errcode":2,
-                "errmsg":"注册此用户名已存在！"
-
-            */
-
-            switch (registerResultInfo.getCode()) {
-
-                case 1:
-                    getView().showMessage(registerResultInfo.getMsg());
-                    getView().NavigationToHome();
-                    break;
-                case 2:
-                    getView().showMessage(registerResultInfo.getMsg());
-                    break;
-
-            }
-
-        }
+        });
 
     }
 
--- app/src/test/java/com/archer/truesure/ExampleUnitTest.java
+++ app/src/test/java/com/archer/truesure/ExampleUnitTest.java
@@ -1,15 +1,64 @@
 package com.archer.truesure;
 
+import com.archer.truesure.net.NetOkHttpClient;
+import com.archer.truesure.user.entity.RegisterInfo;
+import com.google.gson.Gson;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    /**
+     * 测试网络
+     */
+    @Test
+    public void testNet() {
+
+        OkHttpClient okHttpClient = NetOkHttpClient.getInstance().getOkHttpClient();
+
+
+        Gson gson = new Gson();
+        String content = gson.toJson(new RegisterInfo("xuehseng", "ljakshdk"));
+
+        RequestBody body = RequestBody.create(MediaType.parse("tr"), content);
+
+        Request request = new Request.Builder()
+                .url(NetOkHttpClient.APP_URL+"/Handler/UserHandler.ashx?action=login")
+                .post(body)
+                .build();
+
+        Call call = okHttpClient.newCall(request);
+
+        Response execute;
+        String string = null;
+        try {
+            execute = call.execute();
+            string = execute.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals("", string);
+
+    }
+
 }
